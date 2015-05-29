bf529efd8107353ecfe8180b69c0e47184ed6d6b
Camille Casiño <camillecasino@Pauls-MacBook-Pro.local>
2015-05-29 08:17:35 -0400

    c

---
+++ analysis/Compare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Compare {
+
+    public double kPer(double k, double at) {
+	return k/at * 100;
+    }
+
+    public double bPer(double b, double at) {
+	return b/at * 100;
+    }
+}
