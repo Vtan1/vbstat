e71cf4061bb94c7910879ad2f67931eb32595d0c
Victor Tan <victor@gmail.com>
2015-06-11 08:42:24 -0400

    .

--- guistuff/gui.java
+++ guistuff/gui.java
@@ -9,16 +9,17 @@ import java.io.File;
 public class gui extends JFrame implements ActionListener {
     private JPanel stats1,stats2;
     private Container volley;
+    private JDialog error,success;
     private JButton submit;
     private JLabel team1,team2;
     //these are for the first team
-    private JTextArea xok1,xok2,xoa1,xoa2,xmk1,xmk2,xma1,xma2,xss1,xss2,xsa1,xsa2,xld;
+    private JTextArea xok1,xok2,xoa1,xoa2,xmk1,xmk2,xma1,xma2,xmb1,xmb2,xss1,xld;
     //these are for the second team
-    private JTextArea yok1,yok2,yoa1,yoa2,ymk1,ymk2,yma1,yma2,yss1,yss2,ysa1,ysa2,yld;
+    private JTextArea yok1,yok2,yoa1,yoa2,ymk1,ymk2,yma1,yma2,ymb1,ymb2,yss1,yld;
     //these blank fields are for the first team
-    private JTextArea blank1,blank2,blank3,blank4,blank5,blank6,blank7,blank8,blank9,blank10,blank11,blank12,blank13;
+    private JTextArea blank1,blank2,blank3,blank4,blank5,blank6,blank7,blank8,blank9,blank10,blank11,blank12;
     //these blank fields are for the second team
-    private JTextArea blank14,blank15,blank16,blank17,blank18,blank19,blank20,blank21,blank22,blank23,blank24,blank25,blank26;
+    private JTextArea blank14,blank15,blank16,blank17,blank18,blank19,blank20,blank21,blank22,blank23,blank24,blank25;
     
 
     public gui() {
@@ -105,45 +106,37 @@ public class gui extends JFrame implements ActionListener {
 	blank8.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats1.add(blank8);
 
-	xss1 = new JTextArea();
-	xss1.setText("Sets per Set 1");
-	xss1.setEditable(false);
-	stats1.add(xss1);
+	xmb1 = new JTextArea();
+	xmb1.setText("Middle Blocks 1");
+	xmb1.setEditable(false);
+	stats1.add(xmb1);
 	blank9 = new JTextArea();
 	blank9.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats1.add(blank9);
 
-	xss2 = new JTextArea();
-	xss2.setText("Sets per Set 2");
-	xss2.setEditable(false);
-	stats1.add(xss2);
+	xmb2 = new JTextArea();
+	xmb2.setText("Middle Blocks 2");
+	xmb2.setEditable(false);
+	stats1.add(xmb2);
 	blank10 = new JTextArea();
 	blank10.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats1.add(blank10);
 
-	xsa1 = new JTextArea();
-	xsa1.setText("Set Attempts 1");
-	xsa1.setEditable(false);
-	stats1.add(xsa1);
+	xss1 = new JTextArea();
+	xss1.setText("Sets per Set");
+	xss1.setEditable(false);
+	stats1.add(xss1);
 	blank11 = new JTextArea();
 	blank11.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats1.add(blank11);
-
-	xsa2 = new JTextArea();
-	xsa2.setText("Set Attempts 2");
-	xsa2.setEditable(false);
-	stats1.add(xsa2);
-	blank12 = new JTextArea();
-	blank12.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	stats1.add(blank12);
 
 	xld = new JTextArea();
 	xld.setText("Libero Digs");
 	xld.setEditable(false);
 	stats1.add(xld);
-	blank13 = new JTextArea();
-	blank13.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	stats1.add(blank13);
+	blank12 = new JTextArea();
+	blank12.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	stats1.add(blank12);
 
 	
 	stats2 = new JPanel();
@@ -217,54 +210,51 @@ public class gui extends JFrame implements ActionListener {
 	blank21.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats2.add(blank21);
 
-	yss1 = new JTextArea();
-	yss1.setText("Sets per Set 1");
-	yss1.setEditable(false);
-	stats2.add(yss1);
+	ymb1 = new JTextArea();
+	ymb1.setText("Middle Blocks 1");
+	ymb1.setEditable(false);
+	stats2.add(ymb1);
 	blank22 = new JTextArea();
 	blank22.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats2.add(blank22);
 
-	yss2 = new JTextArea();
-	yss2.setText("Sets per Set 2");
-	yss2.setEditable(false);
-	stats2.add(yss2);
+	ymb2 = new JTextArea();
+	ymb2.setText("Middle Blocks 2");
+	ymb2.setEditable(false);
+	stats2.add(ymb2);
 	blank23 = new JTextArea();
 	blank23.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats2.add(blank23);
-
-	ysa1 = new JTextArea();
-	ysa1.setText("Set Attempts 1");
-	ysa1.setEditable(false);
-	stats2.add(ysa1);
+	
+	yss1 = new JTextArea();
+	yss1.setText("Sets per Set");
+	yss1.setEditable(false);
+	stats2.add(yss1);
 	blank24 = new JTextArea();
 	blank24.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	stats2.add(blank24);
-
-	ysa2 = new JTextArea();
-	ysa2.setText("Set Attempts 2");
-	ysa2.setEditable(false);
-	stats2.add(ysa2);
-	blank25 = new JTextArea();
-	blank25.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	stats2.add(blank25);
-
+	
 	yld = new JTextArea();
 	yld.setText("Libero Digs");
 	yld.setEditable(false);
 	stats2.add(yld);
-	blank26 = new JTextArea();
-	blank26.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	stats2.add(blank26);
+	blank25 = new JTextArea();
+	blank25.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	stats2.add(blank25);
 
-	submit = new JButton("Sumbit");
+	submit = new JButton("Submit");
 	volley.add(submit);
+
     }
 
     public void actionPerformed(ActionEvent e) {
 	if (e.getSource() == submit) {
 	    for (int i = 1; i < 27; i++) {
-		if ("blank"
+		if (("blank" + i)(JTextArea).getText().equals("")) {
+     		    //error = new JDialog(error,"Error");
+		    //error.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
+	    }
 	}
     }
 
